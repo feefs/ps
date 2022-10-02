10bf915cdf53467858855b98a72241a37981e396
feefs <feefs@berkeley.edu>
2022-10-02 10:19:50 -0700

    weekly-313

---
+++ leetcode-contests/weekly-313/1.py
@@ -0,0 +1,10 @@
+# 2427. Number of Common Factors
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        if a < b:
+            a, b = b, a
+        result = 0
+        for n in range(1, b + 1):
+            if a % n == 0 and b % n == 0:
+                result += 1
+        return result
---
+++ leetcode-contests/weekly-313/4.py
@@ -0,0 +1,17 @@
+# 2430. Maximum Deletions on a String
+class Solution:
+    def deleteString(self, s: str) -> int:
+        """
+        O(n^3) DP
+        """
+        n = len(s)
+
+        # dp[i] = number of deletions to delete s[i:]
+        dp = [1] * n
+        for i in reversed(range(n)):
+            for width in (i + 1 for i in range((n - i) // 2)):
+                # avoid dp comparisons that can't improve the result
+                if 1 + dp[i + width] > dp[i]:
+                    if s[i : i + width] == s[i + width : i + (2 * width)]:
+                        dp[i] = max(dp[i], 1 + dp[i + width])
+        return dp[0]
