4ebd875b0713da6c5ab3da36e779358f325bd4e0
feefs <feefs@berkeley.edu>
2025-10-26 18:50:40 -0700

    fix problem_number.sh pre-commit script

--- scripts/pre-commit/problem_number.sh
+++ scripts/pre-commit/problem_number.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for problem in "$@"; do
-    if ! head -n 1 "$problem" | grep '^# \d*\. .*$'; then
+    if ! head -n 1 "$problem" | grep -E '^# [0-9]+\. .*$'; then
         echo "$problem is missing a leetcode problem number!"
         exit 1
     fi
